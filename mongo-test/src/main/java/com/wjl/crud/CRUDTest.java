db764fe6910ed93ab56762b9087f98da9c5ec1e3
Jarvis.Wu <shmily_320700@163.com>
2014-11-09 20:47:44 +0800

    增加Mongodb聚合、MapReduce操作等

--- mongo-test/src/main/java/com/wjl/crud/CRUDTest.java
+++ mongo-test/src/main/java/com/wjl/crud/CRUDTest.java
@@ -19,25 +19,39 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
+import com.mongodb.GroupCommand;
+import com.mongodb.MapReduceCommand;
+import com.mongodb.MapReduceOutput;
+import com.mongodb.MapReduceCommand.OutputType;
 import com.mongodb.MongoClient;
 
+/**
+ * 从普通的CRUD到Group和MapReduce等基本使用。
+ * 
+ * @author jl.wu
+ *
+ */
 public class CRUDTest {
 	
-	private final static String Test_DB = "test";
-	private final static String Test_COLL = "message";
+	private final static String DB = "test";
+	private final static String COLL_MSG = "message";
+	private final static String COLL_TEST = "test";
+	private final static String COLL_OUT = "out";
 	private final static String MSG_ID = "_id";
 	private final static String MSG_NAME = "name";
 	private final static String MSG_CONTENT = "content";
 	private MongoClient mongoClient;
 	private DBCollection message;
+	private DBCollection test;
 	
 	@Before
 	public void before(){
 		try {
 			this.mongoClient = new MongoClient("192.168.1.104", 27017);
-			DB db = this.mongoClient.getDB(Test_DB);
-			this.message = db.getCollection(Test_COLL);
-			if(!db.collectionExists(Test_COLL)){
+			DB db = this.mongoClient.getDB(DB);
+			this.test = db.getCollection(COLL_TEST);
+			this.message = db.getCollection(COLL_MSG);
+			if(!db.collectionExists(COLL_MSG)){
 				init();
 			}
 		} catch (UnknownHostException e) {
@@ -98,6 +112,31 @@ public class CRUDTest {
 		DBCursor cur = message.find(new BasicDBObject(), fields);
 		while(cur.hasNext()){
 			print(cur.next().toString());
+		}
+	}
+	
+	/**
+	 * 查询限制 ： limit 、 skip 、 sort 指定字段排序（1是升序 -1降序）
+	 * 
+	 * skip 可以用来做分页，但是当skip的数值较大，跳过太多数据的话，会比较慢。
+	 * 
+	 * var page1 = db.foo.find(cond).limit(15);
+	 * var page2 = db.foo.find(cond).skip(15).limit(15);
+	 * 
+	 * 想不用skip分页，除非查询有类似date之类可以排序的，然后再配合limit。。
+	 * 例如：
+	 * var page = db.food.find({"date":{"$gt": 上一页最后时间}})
+	 *   .sort({"date":-1}).limit(15);
+	 * 
+	 */
+	@Test
+	public void query_all_limit(){
+		DBCursor cur = message.find().limit(5); //只查前5条
+//		DBCursor cur = message.find().skip(5); //跳过5条
+//		DBCursor cur = message.find().limit(10).sort(new BasicDBObject("content", 1));
+		while(cur.hasNext()){
+			print(cur.next().toString());
+			//print((String)message.get("content"));
 		}
 	}
 	
@@ -165,7 +204,12 @@ public class CRUDTest {
 	}
 	
 	/**
-	 * 正则匹配  -- 模糊
+	 * 特定查询系：  正则匹配  -- 模糊
+	 *         
+	 * 其他：
+	 * 内嵌数组查询：$all $size $slice 。 内嵌文档查询等等。
+	 * 
+	 * 
 	 */
 	@Test
 	public void query_pat(){
@@ -173,6 +217,47 @@ public class CRUDTest {
 		DBObject condition = new BasicDBObject(
 				"content", new BasicDBObject("$regex", pattern));
 		DBCursor cur = message.find(condition);
+		while(cur.hasNext()){
+			print(cur.next().toString());
+		}
+	}
+	
+	/**
+	 * 特定查询系：数组查询。
+	 *         
+	 * 测试例子：
+	 * db.message.save({name:"BoBo", content:"haha", games:["cf","lol","tx"]})
+	 * 
+	 * 其他：
+	 * 内嵌数组查询：$all $size    $slice（范围查询） 。 
+	 * 
+	 * 内嵌文档查询 ： $elemMatch 参考《Mongodb权威指南》
+	 * 
+	 */
+	@Test
+	public void query_$all(){
+		DBObject condition = new BasicDBObject("games", 
+				new BasicDBObject("$all", new String[]{"lol", "tx"}));
+//		DBObject condition = new BasicDBObject("games", new BasicDBObject("$size", 3));
+		DBCursor cur = message.find(condition);
+		while(cur.hasNext()){
+			print(cur.next().toString());
+		}
+	}
+	
+	/**
+	 *  where 直接写javascript来查询。 超级强大
+	 *  
+	 *  测试例子：
+	 *  db.test.save([{x:4 ,y:3, z:5}, {x:3 ,y:5, z:4}, {x:3 ,y:3, z:4}])
+	 */
+	@Test
+	public void query_$where(){
+//		DBObject condition = new BasicDBObject("$where", 
+//				"function(){ return this.x == this.y}");
+		DBObject condition = new BasicDBObject("$where", 
+				"function(){ return this.x + this.y + this.z == 12}");
+		DBCursor cur = test.find(condition);
 		while(cur.hasNext()){
 			print(cur.next().toString());
 		}
@@ -200,11 +285,6 @@ public class CRUDTest {
 		DBObject condition = new BasicDBObject(MSG_ID, new ObjectId("545dfea32b3ba519ad9965f9"));
 		DBObject rst = new BasicDBObject(MSG_CONTENT, "wa ha ha...");
 		message.update(condition, rst);
-	}
-	
-	@Test
-	public void count(){
-		print(String.valueOf(message.count()));
 	}
 	
 	/**
@@ -295,6 +375,117 @@ public class CRUDTest {
 		message.remove(new BasicDBObject(MSG_ID, new ObjectId("545dfea32b3ba519ad9965f9")));
 	}
 
+	/**
+	 * 聚合 count
+	 */
+	@Test
+	public void count(){
+		System.out.println(message.count());
+	}
+	
+	/**
+	 * 聚合 distinct
+	 */
+	@Test
+	public void distinct(){
+		System.out.println(message.distinct(MSG_NAME));
+	}
+	
+	/**
+	 * 聚合 group
+	 * 
+	 * 每个文档都会对应调用一次：$reduce = "function(doc,prev){...}
+	 * doc为每次执行的当前文档。
+	 * prev则为累加器文档也就是为最后的结果。
+	 * 分组的每一组都会有自己的累加器，所以结果是不会混淆的。
+	 * 
+	 * 所以这里下面的函数执行，其实就很简单：
+	 * 每次执行一个文档的时候，并没做一些复杂操作，而是累加器做了一次记录数+1.
+	 * 
+	 * condition 可以作条件限制，只处理满足条件的文档集。
+	 * 这里只统计必须有name这个字段的文档。
+	 * 
+	 * finalize 字段是完成器，可以对统计的结果再做计算和修修改改。
+	 * 比如本例，即通过完成器来判断，如果对话记录超过15条，标记为活跃。
+	 * 
+	 * group足以应付普通一般分组，更复杂的可以使用$keyf。
+	 * 或者MapReduce。
+	 * 
+	 */
+	@Test
+	public void group(){
+		BasicDBObject key = new BasicDBObject(MSG_NAME, true);
+		BasicDBObject condition = new BasicDBObject(
+				MSG_NAME, new BasicDBObject("$exists", true));
+		BasicDBObject initial = new BasicDBObject("num", 0);
+		String $reduce = "function(doc,prev){prev.num++; }";
+		String finalize = null;
+//		String finalize = "function(prev){"
+//				              + "if(prev.num > 15)"
+//				              + "     prev.status = 'active';"
+//				              + "else"
+//				              + "     prev.status = 'general';"
+//				           + "}";
+		GroupCommand groupCommand = 
+				new GroupCommand(
+						message, 
+						key, 
+						condition, 
+						initial, 
+						$reduce, 
+						finalize);
+		BasicDBList rst = (BasicDBList)message.group(groupCommand);
+		for(Object obj : rst){
+			System.out.println(obj);
+		}
+	}
+	
+	/**
+	 * 超级强大的聚合工具。  
+	 * 也有点复杂，它执行步骤分为：map、shuffle、reduce。
+	 * 
+	 * MapReduce 它可以轻松并行化	到多个服务器的聚合方法，每个服务器都完成一部分，最后再汇总。
+	 * 使用MapReduce的代价就是速度：group本身就不是很快，MapReduce更慢，绝对不合适"实时"的环境。
+	 * 它的应用场景是后台任务运行MapReduce，然后创建一个保存结果的集合，再对这个集合进行实时查询。
+	 * 
+	 * 例子1：找出集合中所有键，不包含内嵌文档的。
+	 * 输出类型：测试才用OutputType.INLINE ，真正使用建议OutputType.REPLACE输出到集合。
+	 * 
+	 */
+	@Test
+	public void mapReduce_1(){
+		String map = "function(){ for(var key in this){ emit(key, {count:1}); }}";
+		String reduce = "function(key, emits){"
+				       + "  total = 0;" 
+				       + "  for(var i in emits){"
+				          + "   total += emits[i].count;"
+				          + "}"
+				       + "  return {'count': total};"
+				       + "}";
+		MapReduceCommand command = new MapReduceCommand(
+				message, map, reduce, "out_temp", OutputType.INLINE, null);
+		System.out.println(message.mapReduce(command));
+	}
+	
+	@Test
+	public void mapReduce_2(){
+		String map = "function(){ "
+				+ "     if(this.name != 'Jl.Wu') "
+				+ "         return;"
+	            + "     var ws = this.content.split(' ');"
+				+ "     for(var w in ws){ "
+				+ "         emit('words', 1); "
+				+ "     }"
+				+ "}";
+		String reduce = "function(key, emits){"
+				       + "  return emits.length;"
+				       + "}";
+		MapReduceCommand command = new MapReduceCommand(
+				message, map, reduce, "out_temp", OutputType.INLINE, null);
+		System.out.println(message.mapReduce(command));
+	}
+	
+	
 	@After
 	public void after(){
 		if(null != mongoClient)
